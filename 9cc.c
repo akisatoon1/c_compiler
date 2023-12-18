a97db458c83a39579b5732f233bd19ac027ba3ba
akisatoon <akisatoon@gmail.com>
2023-12-18 20:35:14 +0900

    単項+-を追加

--- 9cc.c
+++ 9cc.c
@@ -49,6 +49,7 @@ void gen(Node *);
 Node *primary();
 Node *mul();
 Node *expr();
+Node *unary();
 Node *new_node_num(int);
 Node *new_node(NodeKind, Node *, Node *);
 Token *tokenize(char *p);
@@ -219,22 +220,35 @@ Node *expr()
 
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
     for (;;)
     {
         if (consume('*'))
         {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         }
         else if (consume('/'))
         {
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         }
         else
         {
             return node;
         }
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+    {
+        return primary();
+    }
+    if (consume('-'))
+    {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
 }
 
 Node *primary()
--- test.sh
+++ test.sh
@@ -25,4 +25,5 @@ assert 176 "67 + 83 - 85 + 96 - 34 + 49 "
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
+assert 10 '-10+20'
 echo OK
